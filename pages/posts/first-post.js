a77d39b73fedbb68e9e12275d703031934006c7a
Will Mallett <mallett002@gmail.com>
2020-09-30 07:57:41 -0500

    initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const FirstPost = () => (
+    <div>
+        <h1>First Post.</h1>
+        <Link href='/'><a>Back to home.</a></Link>
+    </div>
+);
+
+export default FirstPost;
